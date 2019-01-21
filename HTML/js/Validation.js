f63581d00001b0ea167c980080f90d85aadaf39a
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-01-21 23:35:28 +0400

    js part

--- HTML/js/Validation.js
+++ HTML/js/Validation.js
@@ -2,8 +2,24 @@ function registration(){
     var name = first.value;
     var surname = last.value;
     var mail = emailaddress.value;
-    var login = login.value;
-    var password = password.value;
+    var login = userId.value;
+    var password = pass.value;
     var password1 = confpassword.value;
+    if(checkName(name)){
+        alert("ok")
+    } 
+
     console.log(last.value)
+}
+
+function checkName(name){
+    if(name.length == 0){
+        document.getElementsByClassName("error")[0].innerHTML = "!!! Please enter your first name";
+        return false;
+     } //else if(){
+
+    // } 
+    else {
+        return true;
+    }
 }
