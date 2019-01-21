5fe32b960f60e1229d986fa34ab90cfcc6d23f3b
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-01-21 13:16:33 +0400

    registration

---
+++ HTML/js/Validation.js
@@ -0,0 +1,9 @@
+function registration(){
+    var name = first.value;
+    var surname = last.value;
+    var mail = emailaddress.value;
+    var login = login.value;
+    var password = password.value;
+    var password1 = confpassword.value;
+    console.log(last.value)
+}
