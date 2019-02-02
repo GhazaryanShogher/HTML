a681106728e3441270caf2befcf5aeaddc3ab84c
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-02 16:09:02 +0400

    part

--- HTML/js/promise.js
+++ HTML/js/promise.js
@@ -2,9 +2,10 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 .then(response=> response.json() 
     .then(data=>{
     for(let i=0; i<5; i++){
+        console.log(data[i])
         for(key in data[i]){
         let lis = document.createElement('li');
-        var text= document.createTextNode(data[i][key]);
+        var text= document.createTextNode(JSON.stringify(data[i]));
         obj.appendChild(lis);
         lis.appendChild(text);  
         }        
