81c4117efb03ae12e1768aefd7ad4de06c5b6ce6
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-02 13:30:56 +0400

    comit changes

---
+++ HTML/js/clouser.js
@@ -0,0 +1,10 @@
+function addCount() {
+    let num = 1;
+    function incr(){
+      return num++;
+    }
+    return incr;
+  }
+  let incr =  addCount();
+  console.log(incr());
+  
---
+++ HTML/js/lexical.js
@@ -0,0 +1,8 @@
+let age = "k"'
+function print(){
+    function log(){
+        console.log(age)
+    }
+    log(age);
+}
+print()
