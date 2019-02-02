583cf449d974a20c4a2bd710f2a3157bb530c14f
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-02 11:06:47 +0400

    ERROR

--- HTML/js/todo.js
+++ HTML/js/todo.js
@@ -1,8 +1,34 @@
-function add(){
-    var inp = add.value;
+
+//  let list = ["ajjaj"];
+//  list.unshift("kdshcfif");
+function addToDoItem() {
+    let list = ["ajjaj"];
+    let inp = todo.value;
     if(inp.length == 0){
-        return "Please fill the input";
+        alert("Please fill the input");
     } else{
-        var list = [];
-    }
+        function unshift(){
+            list.unshift(inp);
+            console.log(list);
+            return list;
+        }        
+    } 
+    
+    
 }
+  let arrToDo =  addToDoItem();
+  console.log(arrToDo())
+  
+// function add(){
+//     let inp = todo.value;
+//     if(inp.length == 0){
+//         alert("Please fill the input");
+//     } else{ 
+//         list.unshift(inp);
+//         console.log(list)
+//         return list;
+//     }
+//     console.log("list", list)
+// }
+// console.log(list);
+
