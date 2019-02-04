5c7b2a2b5d8c3700636b3974bbf79c8a9b6a9a1f
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-04 13:56:41 +0400

    some changes

--- HTML/js/todo.js
+++ HTML/js/todo.js
@@ -1,4 +1,5 @@
 let list = [];
+//adding new to do to existing list
 function addToDoItem() {
     if(todo.value == ''){
         alert("Please fill the input");
@@ -8,17 +9,32 @@ function addToDoItem() {
     main.innerHTML = '';  
     createItem();   
 } 
-console.log('list',list);
-
+//creating todo div
 function createItem(){ 
     for(let i = 0; i < list.length; i++) {         
     var container = document.createElement('div');
+    var i1 = document.createElement('i');
+    i1.className = ("far fa-check-square fa-2x green-color");
+    i1.addEventListener("click", changeColor);
+    var i2 = document.createElement('i');
+    i2.className = ("far fa-trash-alt f2ed fa-2x");
     container.innerHTML = '';
         container.className = ('main')
-        var sp = document.createElement('span');
+        var sp = document.createElement('div');
         var text = document.createTextNode(list[i]);
         sp.appendChild(text) ;
+        container.appendChild(i1);
         container.appendChild(sp);
+        container.appendChild(i2);
         main.appendChild(container);
     }
+}
+//will change the color of chacked item after clicking on it
+function changeColor(){
+   this.style.color = "red";
+
+}
+//deleteing from todo list
+function del(){
+
 }
