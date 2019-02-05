b9154b0ee2009da0e46af825dea15b9ac736ce4c
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-05 17:34:44 +0400

    text decoration

--- HTML/js/todo.js
+++ HTML/js/todo.js
@@ -17,7 +17,8 @@ function createItem(){
     i1.className = ("far fa-check-square fa-2x green-color");
     i1.addEventListener("click", changeColor);
     var i2 = document.createElement('i');
-    i2.className = ("far fa-trash-alt f2ed fa-2x");
+    i2.className = ("far fa-trash-alt f2ed fa-2x");    
+    i2.addEventListener("click", del);
     container.innerHTML = '';
         container.className = ('main')
         var sp = document.createElement('div');
@@ -31,11 +32,22 @@ function createItem(){
     }
 }
 //will change the color of chacked item after clicking on it
+let binColor = true;
 function changeColor(){
-   this.style.color = "red";
-
+    if(binColor){
+        this.style.color = "red";        
+        this.nextSibling.style.color = "red";
+        this.nextSibling.style.textDecoration = "line-through";
+        binColor = false;
+        return;
+    } else {
+        this.style.color = "green";
+        this.nextSibling.style.color = "green";
+        this.nextSibling.style.textDecoration = "none";
+        binColor = true;
+    }  
 }
 //deleteing from todo list
 function del(){
-
+    this.parentNode.remove(this.parentNode);
 }
