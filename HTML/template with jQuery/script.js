998ffc8ce4660671e627aa6540a62ee156939dbb
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-12 17:52:39 +0400

    function

---
+++ HTML/template with jQuery/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.next').on('click', function(){
+        var first = $('.active');
+        var second = first.next();
+
+        if(second.length){
+            first.removeClass('active');
+            second.addClass('active');
+        }
+    });
+    $('.prev').on('click', function(){
+        var first = $('.active');
+        var second = first.prev();
+
+        if(second.length){
+            first.removeClass('active');
+            second.addClass('active');
+        }
+    })
+});
