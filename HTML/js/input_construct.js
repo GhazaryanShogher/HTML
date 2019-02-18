699f1484ce542dd1ced4de5d253b36ac22e16f6e
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-18 23:59:21 +0400

    create

--- HTML/js/input_construct.js
+++ HTML/js/input_construct.js
@@ -3,5 +3,11 @@ function Create(tag,color,text,background){
     this.color = color;
     this.text = text;
     this.background = background;
+    this.create = function(){
+        var elem = document.createElement('this.tag');
+        elem.style.color = this.color;
+        elem.innerHTML = this.text;
+        elem.style.background = this.background;
+    }
 
 }
