5a720309afe236f65ea22e2e4cca596b223a0450
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-19 19:37:10 +0400

    comments

--- HTML/js/input_construct.js
+++ HTML/js/input_construct.js
@@ -1,13 +1,14 @@
+//constructor
 function Create(tag,color,text,background){
     this.tag = tag;
     this.color = color;
     this.text = text;
     this.background = background;
     this.create = function(){
-        var elem = document.createElement(tag1);
-        elem.style.color = color1;
-        elem.innerHTML = text1;
-        elem.style.background = background1;
+        var elem = document.createElement(this.tag);
+        elem.style.color = this.color;
+        elem.innerHTML = this.text;
+        elem.style.background = this.background;
         elem.className = 'sizes';
         container.appendChild(elem);
     }
@@ -15,6 +16,7 @@ function Create(tag,color,text,background){
 // function Creator(tag,color,text,background){
 //     Create.call(this,tag,color,text,background);
 // }
+//call
  function creator(){debugger
     let tag1 = content.value;
     let color1 = color.value;
