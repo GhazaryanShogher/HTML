527c1a26fc5c8943c29dc483a22f294c9e4a971f
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-19 16:58:11 +0400

    need  to think

--- HTML/js/input_construct.js
+++ HTML/js/input_construct.js
@@ -4,10 +4,33 @@ function Create(tag,color,text,background){
     this.text = text;
     this.background = background;
     this.create = function(){
-        var elem = document.createElement('this.tag');
-        elem.style.color = this.color;
-        elem.innerHTML = this.text;
-        elem.style.background = this.background;
+        var elem = document.createElement(tag1);
+        elem.style.color = color1;
+        elem.innerHTML = text1;
+        elem.style.background = background1;
+        elem.className = 'sizes';
+        container.appendChild(elem);
     }
-
 }
+// function Creator(tag,color,text,background){
+//     Create.call(this,tag,color,text,background);
+// }
+ function creator(){debugger
+    let tag1 = content.value;
+    let color1 = color.value;
+    let text1 = text.value;
+    let background1 = fon.value;
+    var elemen = new Create(tag1,color1,text1,background1);
+    Create.call(elemen);
+    elemen.create();
+ }
+
+ 
+// function result(tag1,color1,text1,background1){
+//     var elem = document.createElement(tag1);
+//     elem.style.color = color1;
+//     elem.innerHTML = text1;
+//     elem.style.background = background1;
+//     elem.className = 'sizes';
+//     container.appendChild(elem);
+// }
