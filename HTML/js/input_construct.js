f3f821686a26d2bff69499d801caca59558b5099
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-20 23:33:15 +0400

    creating element with inputs values

--- HTML/js/input_construct.js
+++ HTML/js/input_construct.js
@@ -16,6 +16,15 @@ class Element {
     }
 
 }
+//call
+function creator(){
+    let tag1 = content.value;
+    let color1 = color.value; 
+    let text1 = text.value;
+    let background1 = fon.value;
+    var elemen = new Element(tag1,color1,text1,background1);
+    elemen.elemCreate();
+ }
 // function Create(tag,color,text,background){
 //     this.tag = tag;
 //     this.color = color;
@@ -32,23 +41,4 @@ class Element {
 // }
 // function Creator(tag,color,text,background){
 //     Create.call(this,tag,color,text,background);
-// }
-//call
- function creator(){
-    let tag1 = content.value;
-    let color1 = color.value; 
-    let text1 = text.value;
-    let background1 = fon.value;
-    var elemen = new Element(tag1,color1,text1,background1);
-    elemen.elemCreate();
- }
-
- 
-// function result(tag1,color1,text1,background1){
-//     var elem = document.createElement(tag1);
-//        elem.style.color = color1;
-//     elem.innerHTML = text1;
-//     elem.style.background = background1;
-//     elem.className = 'sizes';
-//     container.appendChild(elem);
 // }
